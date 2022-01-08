e698d48b2ee43366c6be26a5ff9ce3963ace48cd
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-01-08 16:46:51 +0100

    Removed shop app code for clean slate

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
 import "./App.css";
-import spiderman from "../src/spiderman.png"
+// import spiderman from "../src/spiderman.png"
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Bobs and Bits</h1>
-    </header>
-  );
-}
-function Main(props) {
-  return (
-    <section>
-      <p>We sell {props.adjective} things. Very weird things.</p>
-      <img src={spiderman} alt="Spider-man Logo" height="150px" />
-      <ul style={{ listStyleType: "none" }}>
-        {props.items.map((item) => <li key={item.id}>{item.title}</li>)}
-      </ul>
-    </section>
-  );
-}
-function Footer(props) {
-  return (
-    <section>
-      <p>Seriously. It's {props.year}</p>
-    </section>
-  );
-}
+// function Header(props) {
+//   return (
+//     <header>
+//       <h1>{props.name}'s Bobs and Bits</h1>
+//     </header>
+//   );
+// }
+// function Main(props) {
+//   return (
+//     <section>
+//       <p>We sell {props.adjective} things. Very weird things.</p>
+//       <img src={spiderman} alt="Spider-man Logo" height="150px" />
+//       <ul style={{ listStyleType: "none" }}>
+//         {props.items.map((item) => <li key={item.id}>{item.title}</li>)}
+//       </ul>
+//     </section>
+//   );
+// }
+// function Footer(props) {
+//   return (
+//     <section>
+//       <p>Seriously. It's {props.year}</p>
+//     </section>
+//   );
+// }
 
-const items = [
-  "Fishing rod", 
-  "Cheese sandwich", 
-  "Pokemon cards"
-];
+// const items = [
+//   "Fishing rod", 
+//   "Cheese sandwich", 
+//   "Pokemon cards"
+// ];
 
-//Transform array of strings into array of objects with ids
-const itemObjects = items.map((item, i) => ({id: i, title: item}));
+// //Transform array of strings into array of objects with ids
+// const itemObjects = items.map((item, i) => ({id: i, title: item}));
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Header name="Ben" />
+//         <Main adjective="stanky" items={itemObjects}/>
+//         <Footer year={new Date().getFullYear()} />
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header name="Ben" />
-        <Main adjective="stanky" items={itemObjects}/>
-        <Footer year={new Date().getFullYear()} />
-      </header>
+      Components go here
     </div>
   );
 }
