b8822b14f06d5230301e47d5b85a293fb0867b90
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-01-09 19:26:35 +0100

    Added links between routes on main page, and a handler for nonexistent pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useReducer } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Contact } from "./pages";
+import { Home, About, Events, Contact, Whoops404 } from "./pages";
 // import spiderman from "../src/spiderman.png"
 
 // function Header(props) {
@@ -217,6 +217,7 @@ function App() {
         <Route path="/about" element={<About />}  />
         <Route path="/events" element={<Events />}  />
         <Route path="/contact" element={<Contact />}  />
+        <Route path="*" element={<Whoops404 />}  />
       </Routes>
     </div>
     );
